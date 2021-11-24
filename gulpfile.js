a3b1b219b28f911a29e5b1df86c80eef01b44e8a
Paul Johnson <pmj@umbraco.com>
2021-11-24 20:19:09 +0000

    Call invalidate caches endpoint on *.md changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 const { watch, series } = require('gulp');
 const browsersync = require('browser-sync').create();
+const axios = require('axios');
 
-function browsersyncInit(done){
+function browsersyncInit(done) {
   browsersync.init({
-      proxy: 'http://localhost:5000',
-      port: 5001
+    proxy: 'localhost:5000',
+    port: 5001
   });
   done();
 }
 
 function reload(done) {
-  browsersync.reload();
-  done();
+  const url = `${browsersync.getOption('urls').get('local')}/documentation/caches`;
+
+  axios.delete(url)
+    .then(() => {
+      browsersync.reload();
+      done();
+    });
 }
 
 function watchMarkdown() {
