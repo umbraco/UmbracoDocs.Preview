2331954c0e01ff19440b24cff662574e0e41844a
Paul Johnson <pmj@umbraco.com>
2021-11-28 19:01:58 +0000

    Combine notification handlers, they are related and order is important.

--- src/Umbraco.Docs.Preview.App/Messaging/Notifications/DocumentationUpdated/DocsUpdatedHandler.cs
+++ src/Umbraco.Docs.Preview.App/Messaging/Notifications/DocumentationUpdated/DocsUpdatedHandler.cs
@@ -7,23 +7,30 @@ using Umbraco.Docs.Preview.App.Services;
 
 namespace Umbraco.Docs.Preview.App.Messaging.Notifications.DocumentationUpdated
 {
-    public class InvalidateDocsTreeCache : INotificationHandler<DocsUpdated>
+    public class DocsUpdatedHandler : INotificationHandler<DocsUpdated>
     {
         private readonly ILogger _log;
         private readonly IMemoryCache _memoryCache;
+        private readonly IDocumentationChangeNotifier _clientNotifier;
 
-        public InvalidateDocsTreeCache(
-            ILogger<InvalidateDocsTreeCache> log,
-            IMemoryCache memoryCache)
+        public DocsUpdatedHandler(
+            ILogger<DocsUpdatedHandler> log,
+            IMemoryCache memoryCache,
+            IDocumentationChangeNotifier clientNotifier)
         {
             _log = log;
             _memoryCache = memoryCache;
+            _clientNotifier = clientNotifier;
         }
 
         public Task Handle(DocsUpdated notification, CancellationToken cancellationToken)
         {
-            _log.LogDebug("Clearing DocsTree cache.");
+            _log.LogDebug("Documentation updated clearing DocsTree cache.");
             _memoryCache.Remove(nameof(IDocumentService.GetDocsTree));
+
+            _log.LogInformation("Documentation updated, triggering UI reload.");
+            _clientNotifier.PublishChangeNotifications();
+         
             return Task.CompletedTask;
         }
     }
